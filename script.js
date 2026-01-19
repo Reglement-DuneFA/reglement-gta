b20427c8368233eb70abe7c42b255882ecf588d0
Reglement-DuneFA <Elkanfaouimehdi94@gmail.com>
2026-01-19 22:53:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSection(id) {
+    document.querySelectorAll('.page').forEach(page => {
+        page.classList.remove('active');
+    });
+
+    document.getElementById(id).classList.add('active');
+}
